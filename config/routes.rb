d0f86d80d921e4f3c8f6b8893a0bbfee06f83142
Ronalde Monezzi <ronalde.monezzi@gmail.com>
2023-09-14 12:19:24 -0300

    destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  get "/blog_posts/new", to: "blog_posts#new", as: :new_blog_post
-  get "/blog_posts/:id", to: "blog_posts#show", as: :blog_post
-  patch "/blog_posts/:id", to: "blog_posts#update"
-  get "blog_posts/:id/edit", to: "blog_posts#edit", as: :edit_blog_post
-  post "/blog_posts", to: "blog_posts#create" , as: :blog_posts
+  # get "/blog_posts/new", to: "blog_posts#new", as: :new_blog_post
+  # get "/blog_posts/:id", to: "blog_posts#show", as: :blog_post
+  # patch "/blog_posts/:id", to: "blog_posts#update"
+  # delete "/blog_posts/:id", to: "blog_posts#destroy"
+  # get "blog_posts/:id/edit", to: "blog_posts#edit", as: :edit_blog_post
+  # post "/blog_posts", to: "blog_posts#create" , as: :blog_posts
+  # get "/blog_posts", to: "blog_posts#index
+  #The routes above are the same as the "resources :blog_posts" below but we still need to keep the root path route.
+
+  resources :blog_posts
 
   # Defines the root path route ("/")
   root "blog_posts#index"
